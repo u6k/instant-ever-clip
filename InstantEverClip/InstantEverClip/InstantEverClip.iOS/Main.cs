18fca42456f08a8fadf03d5a7115a3df6b17f215
u6k <u6k.apps@gmail.com>
2016-04-19 16:06:46 +0900

    iOSアプリ起動時にToastsプラグインを使用可能にしました。

--- InstantEverClip/InstantEverClip/InstantEverClip.iOS/Main.cs
+++ InstantEverClip/InstantEverClip/InstantEverClip.iOS/Main.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using Plugin.Toasts;
+using Xamarin.Forms;
+
 namespace InstantEverClip.iOS
 {
     public class Application
@@ -12,6 +15,9 @@ namespace InstantEverClip.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            DependencyService.Register<ToastNotificatorImplementation>();
+            ToastNotificatorImplementation.Init();
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
